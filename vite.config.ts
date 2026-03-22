c1bf6c6dec611c8b2edfa4b0e642a3d6cd4eb793
Rolling <rollingwilhoite563@gmail.com>
2026-03-22 11:48:05 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      base: '/G-AI-Studio-Avrora/',
       plugins: [react(), tailwindcss()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
